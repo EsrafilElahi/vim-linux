2a6503ef7fcf5650255da9f8b528cadbdf976fd2
Esrafil <essinho1121@gmail.com>
2023-07-08 11:58:01 +0330

    Update terminal_shortcuts.sh

--- terminal_shortcuts.sh
+++ terminal_shortcuts.sh
@@ -4,7 +4,10 @@ ctrl+shift+v : pase the text into terminal
 ctrl+r : search the history commands
 ctrl+f : find the text into terminal
 
+ctrl+u: Delete all contents to the left of the cursor
+
 ls : Lists all files and directories in the present working directory
 ls -a : Lists all files and directories in the present working directory with hidden fils and folders
 l : Lists all files and directories in the present working directory (indicates that which is folder and which is file)
 
+cat file.txt : show contents pf file.txt
